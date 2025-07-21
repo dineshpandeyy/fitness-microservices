4f73cbe963b6c44f7ae000fe1bd508818427bad2
dineshpandeyy <dineshpandey3536@gmail.com>
2025-07-21 00:01:11 -0500

    sending activity data to rabbitmq

--- activityservice/src/main/java/com/fitness/activityservice/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityService.java
@@ -4,6 +4,9 @@ import com.fitness.activityservice.dto.ActivityRequest;
 import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.model.Activity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,10 +14,18 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
 
@@ -34,6 +45,14 @@ public class ActivityService {
 
         Activity savedActivity = activityRepository.save(activity);
 
+        // Publish to rabbitmq for AI processing
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+
+        } catch (Exception e) {
+            log.error("Failed to publish activity to Rabbitmq");
+
+        }
         return mapToResponse(savedActivity);
     }
 
--- aiservice/src/main/java/com/example/aiservice/MongoConfig.java
+++ aiservice/src/main/java/com/example/aiservice/MongoConfig.java
@@ -1,4 +1,4 @@
-package com.fitness.aiservice.config;
+package com.example.aiservice;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
